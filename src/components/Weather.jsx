b3a2e89dad0b874a87d725955b52327bf95c9610
StorfenGIT <johber01@gmail.com>
2024-08-29 01:12:59 +0200

    Removing stuff

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Weather.css'
+import search_icon from '../assets/search.png'
 
 const Weather = () => {
     return (
         <div className='weather'>
             <div className='search-baar'>
                 <input type='text' placeholder='Search'/>
-                <img src='' alt='' />
+                <img src={search_icon} alt='' />
             </div>
         </div>
     )

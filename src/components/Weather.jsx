62c22534a8b6bfb01c510b8f6bef9db849f4d199
StorfenGIT <johber01@gmail.com>
2024-08-29 01:02:27 +0200

    Developing fundmentals

---
+++ src/components/Weather.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Weather.css'
+
+const Weather = () => {
+    return (
+        <div className='weather'>
+            <div className='search-baar'>
+                <input type='text' placeholder='Search'/>
+                <img src='' alt='' />
+            </div>
+        </div>
+    )
+}
+export default Weather
